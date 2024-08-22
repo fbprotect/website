d61673105678794b79eebadf8a725afb0585a186
fbprotect <166386929+fbprotect@users.noreply.github.com>
2024-08-23 02:35:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function v(){
+    var i = document.getElementById("inav");
+
+    if (i.className == "lnks"){
+        i.className = "lnks lnksv";
+    }
+    else {
+        i.className = "lnks";
+    }
+}
